c8d258ca17818b5318b754098abec54049efcd1d
Lalrinawma <terinao86@gmail.com>
2022-01-10 17:05:43 +0530

    Brand Category Image Product and Variant added

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    protected $with = ['brand', 'category', 'variant'];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class, 'brand_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function variant()
+    {
+        return $this->hasMany(Variant::class, 'product_id');
+    }
+}
