32088ac03d7fa11e7ed02fbc8b527336e82f3a86
Lalrinawma <terinao86@gmail.com>
2022-01-15 16:26:38 +0530

    AddressController added with function update delete store and all

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AddressController extends Controller
+{
+    //
+    public function all(Request $request)
+    {
+        return $request->user()->addresses;
+    }
+    public function store(Request $request)
+    {
+        $user = $request->user();
+        $validated = $request->validate([
+            'name' => 'required',
+            'phone_number' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+            'district' => 'required',
+            'street' => 'required',
+            'landmark' => 'required',
+            'house_no' => 'required',
+            'pincode' => 'required'
+        ]);
+        Address::create(array_merge($validated, ['user_id' => $user->id]));
+        return redirect()->back();
+    }
+    public function edit(Address $address)
+    {
+        return Inertia::render('EditAddress', ['address' => $address]);
+    }
+    public function update(Address $address, Request $request)
+    {
+        $user = $request->user();
+        $validated = $request->validate([
+            'name' => 'required',
+            'phone_number' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+            'district' => 'required',
+            'street' => 'required',
+            'landmark' => 'required',
+            'house_no' => 'required',
+            'pincode' => 'required'
+        ]);
+        $address->update($validated);
+        return redirect()->back();
+    }
+}
