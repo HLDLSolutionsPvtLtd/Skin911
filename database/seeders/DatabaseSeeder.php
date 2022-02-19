b85b7ff10d4416a25556738180b377285e11646e
Lalrinawma <terinao86@gmail.com>
2022-02-19 15:20:55 +0530

    seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::insert([
+            'name' => 'admin',
+            'email' =>'admin@test.com',
+            'password' => Hash::make('password'),
+            'phone_no' => Str::random(10),
+            'is_admin' => '1',
+            'address'  => Str::random(10),
+        ]);
     }
 }
