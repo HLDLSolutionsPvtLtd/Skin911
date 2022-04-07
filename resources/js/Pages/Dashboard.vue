b1d2f53f438380c15503338261edc65b926863dc
Lalrinawma <terinao86@gmail.com>
2022-04-07 16:35:11 +0530

    bug fixes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -42,8 +42,8 @@
                     <div class="ribbon2 ribbon2-top-left">
                         <span>DISCOUNT</span>
                     </div>
-                    <i @click="discount_popup=!discount_popup" class="absolute -right-5 rounded-full border p-1 border-gray-100 -top-5 cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-gray-100 hover:text-red-500" width="15" height="15" viewBox="0 0 24 24">
+                    <i @click="discount_popup=!discount_popup" class="absolute -right-5 rounded-full border p-1 border-gray-400 -top-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-gray-600 hover:text-red-500" width="15" height="15" viewBox="0 0 24 24">
                             <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/>
                         </svg>
                     </i>
